2315c3d0be558f807d65a467395f094b879bda78
xakepa <plamen_ivanov@mail.com>
2020-07-29 12:17:04 +0300

    small initial changes

---
+++ ads/src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './index.module.css'
+
+const Header = () => {
+    return (
+        <nav className={styles.navi}>
+            FUUUUCK
+        </nav>
+    )
+
+}
+
+export default Header
