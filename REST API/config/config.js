2450ec058f8494f69935488f2639acbf0dfffa0c
xakepa <plamen_ivanov@mail.com>
2020-08-06 22:51:35 +0300

    change cookie name

--- REST API/config/config.js
+++ REST API/config/config.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
     development: {
-        port: process.env.PORT || 9999,
+        port: process.env.PORT || 6666,
         dbURL: process.env.DB_URL,
         authCookieName: 'jwt-token'
     },
